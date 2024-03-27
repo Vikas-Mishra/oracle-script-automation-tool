71c0df9ab43e703e85135d14eee5c7c5a202a702
Vikas-Mishra <mishra.vikas887@gmail.com>
2024-03-27 13:42:19 +0530

    Update script-runner.sql

--- script-runner.sql
+++ script-runner.sql
@@ -1,19 +1,19 @@
-CONNECT user/"password"@domainname1.domain.COM
-SPOOL Logs\dbuser1_domainurl.txt
+CONNECT user/"password"@DbHostName1
+SPOOL Logs\@DbHostName1.txt
 SET LINESIZE 100
 SET PAGESIZE 50
 @"%scriptpath%"
 SPOOL OFF
 
-CONNECT user/"password"@domainname2.domain.COM
-SPOOL Logs\dbuser2_domainurl.txt
+CONNECT user/"password"@DbHostName2
+SPOOL Logs\@DbHostName2.txt
 SET LINESIZE 100
 SET PAGESIZE 50
 @"%scriptpath%"
 SPOOL OFF
 
-CONNECT user/"password"@domainname3.domain.COM
-SPOOL Logs\dbuser3_domainurl.txt
+CONNECT user/"password"@DbHostName3
+SPOOL Logs\@DbHostName3.txt
 SET LINESIZE 100
 SET PAGESIZE 50
 @"%scriptpath%"
